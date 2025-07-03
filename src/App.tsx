e62c3e430d6f8485587d9e887d98d75d10db0cba
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-03 20:42:04 -0300

    feat(tailwind): install and setup tailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
 function App() {
   return (
-    <>
-      <h1 className="">AULA TAILWINDCSS </h1>
-    </>
+    <div className="flex h-screen flex-col items-center justify-center">
+      <form className="flex flex-col gap-3">
+        <label htmlFor="nome" className="font-bold">
+          Nome
+        </label>
+        <input
+          type="text"
+          placeholder="informe seu nome"
+          className="border-1 h-10 border-red-500 px-0.5 font-semibold"
+        />
+        <label htmlFor="" className="font-bold">
+          Email
+        </label>
+        <input
+          type="text"
+          placeholder="informe seu email"
+          className="border-1 h-10 border-red-500 px-0.5 font-semibold"
+        />
+
+        <button type="submit" className="w-[60px] rounded bg-green-500">
+          Enviar
+        </button>
+      </form>
+    </div>
   );
 }
 
